c5107ba0b7d99826269c642c06dfab4a6acaa20e
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-31 03:02:17 +0300

    add banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Banner from "@/components/Banner";
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className={inter.className}>Hello world</div>;
+  return (
+    <div className={inter.className}>
+      <Banner />
+    </div>
+  );
 }
