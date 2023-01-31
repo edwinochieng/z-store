e51727c6fc00045fec1f77dc07460c8f2e20f2f2
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-31 23:42:09 +0300

    add Quicksand font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
+import { Quicksand } from "@next/font/google";
+
+const quicksand = Quicksand({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -9,7 +12,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head />
-      <body className='max-w-[1920px] w-full px-4 md:px-12 lg:px-16'>
+      <body
+        className={`max-w-[1920px] w-full px-4 md:px-12 lg:px-16 ${quicksand.className}`}
+      >
         <Navbar />
         <div>{children}</div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import Banner from "@/components/Banner";
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className={inter.className}>
+    <div>
       <Banner />
     </div>
   );
