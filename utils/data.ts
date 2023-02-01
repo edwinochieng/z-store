142d96697bbb884d5153252400482a0f644f427f
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-01 21:59:38 +0300

    categories data and types

--- utils/data.ts
+++ utils/data.ts
@@ -6,6 +6,45 @@ image:string
 category:string
 }
 
+interface Categories {
+    name:string
+    value:string
+    image:string
+}
+
+export const categories:Categories[] = [
+    {
+        name:'Shirts',
+        value:'shirts',
+        image:'/images/products/shirt12.jpg'
+    },
+    {
+        name:'Bags',
+        value:'bags',
+        image:'/images/products/bag3.jpg'
+    },
+    {
+        name:'Shoes',
+        value:'shoes',
+        image:'/images/products/shoe12.jpg'
+    },
+    {
+        name:'Watches',
+        value:'watches',
+        image:'/images/products/watch3.jpg'
+    },
+    {
+        name:'Jackets',
+        value:'jackets',
+        image:'/images/products/jacket2.jpg'
+    },
+    {
+        name:'Accessories',
+        value:'accessories',
+        image:'/images/products/accessory6.jpg'
+    },
+]
+
 export const products:Products[] = [
 {
     id: 0,
