39ff82196bfa42f0858ccdbdc2e2e1170664fe3a
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-01 21:59:10 +0300

    refactor code

--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { categories } from "@/utils/data";
 
 export const Categories = () => (
   <DropdownMenu.Root>
@@ -9,24 +10,14 @@ export const Categories = () => (
     </DropdownMenu.Trigger>
     <DropdownMenu.Portal>
       <DropdownMenu.Content className='bg-white w-[180px] p-1 rounded shadow-lg '>
-        <DropdownMenu.Item className='pl-4 rounded hover:bg-cyan-200 hover:outline-none'>
-          Shirts
-        </DropdownMenu.Item>
-        <DropdownMenu.Item className='pl-4 rounded hover:bg-cyan-200 hover:outline-none'>
-          Watches
-        </DropdownMenu.Item>
-        <DropdownMenu.Item className='pl-4 rounded hover:bg-cyan-200 hover:outline-none'>
-          Bags
-        </DropdownMenu.Item>
-        <DropdownMenu.Item className='pl-4 rounded hover:bg-cyan-200 hover:outline-none'>
-          Jackets
-        </DropdownMenu.Item>
-        <DropdownMenu.Item className='pl-4 rounded hover:bg-cyan-200 hover:outline-none'>
-          Shoes
-        </DropdownMenu.Item>
-        <DropdownMenu.Item className='pl-4 rounded hover:bg-cyan-200 hover:outline-none'>
-          Accessories
-        </DropdownMenu.Item>
+        {categories.map((category) => (
+          <DropdownMenu.Item
+            key={category.name}
+            className='pl-4 rounded hover:bg-cyan-200 hover:outline-none'
+          >
+            {category.name}
+          </DropdownMenu.Item>
+        ))}
       </DropdownMenu.Content>
     </DropdownMenu.Portal>
   </DropdownMenu.Root>
