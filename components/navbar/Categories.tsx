3043ebb298c4312a34d56fc8de405453fdd37826
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-02 20:39:38 +0300

    style navbar

--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -2,19 +2,27 @@
 
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { categories } from "@/utils/data";
+import Image from "next/image";
 
 export const Categories = () => (
   <DropdownMenu.Root>
-    <DropdownMenu.Trigger className='outline-none'>
+    <DropdownMenu.Trigger className='outline-none font-medium text-base hover:text-gray-400'>
       Categories
     </DropdownMenu.Trigger>
     <DropdownMenu.Portal>
-      <DropdownMenu.Content className='bg-white w-[180px] p-1 rounded shadow-lg '>
+      <DropdownMenu.Content className='bg-white w-[200px] p-1 rounded shadow-lg '>
         {categories.map((category) => (
           <DropdownMenu.Item
             key={category.name}
-            className='pl-4 rounded hover:bg-cyan-200 hover:outline-none'
+            className='outline-none flex items-center font-medium text-sm h-[40px] mt-0.5 px-1 rounded cursor-pointer hover:bg-gray-100'
           >
+            <Image
+              src={category.image}
+              height={40}
+              width={40}
+              alt={category.name}
+              className='pr-0.5 h-full'
+            />
             {category.name}
           </DropdownMenu.Item>
         ))}
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,13 +4,16 @@ import React from "react";
 import { Categories } from "./Categories";
 import { Account } from "./Account";
 import { IoBagHandle } from "react-icons/io5";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className='bg-transparent'>
       <div className='flex justify-between w-full py-2 z-10'>
         <div className='flex items-center'>
-          <h1 className='font-bold'>Z-Store</h1>
+          <Link href='/'>
+            <h1 className='font-bold text-xl'>Z-Store</h1>
+          </Link>
           <div className='pl-5'>
             <Categories />
           </div>
@@ -20,7 +23,9 @@ export default function Navbar() {
             <Account />
           </div>
           <div className='pl-5'>
-            <IoBagHandle size={24} />
+            <Link href='/cart'>
+              <IoBagHandle size={24} />
+            </Link>
           </div>
         </div>
       </div>
