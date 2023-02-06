b468c9831d8232092a69ed788a435db74c81c979
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-06 23:38:27 +0300

    persist store

--- store/store.ts
+++ store/store.ts
@@ -1,5 +1,6 @@
 import { Products } from '@/utils/data'
 import { create } from 'zustand'
+import {persist} from 'zustand/middleware'
 
 interface Store {
   cartItems: Products[]
@@ -7,17 +8,22 @@ interface Store {
   addToCart: (product:Products) => void
 }
 
-const useStore = create<Store>((set) => ({
-  cartItems: [],
-  quantity:0,
-  addToCart: (product) => {
-    set(state => (
-        {
-            quantity: state.quantity + 1,
-            cartItems:[...state.cartItems, product]
-        }
-    ))
-  }
-}))
+const useStore = create<Store>()(
+    persist(
+        (set) => ({
+            cartItems: [],
+            quantity:0,
+            addToCart: (product) => {
+              set((state) => (
+                  {
+                      quantity: state.quantity + 1,
+                      cartItems:[...state.cartItems, product]
+                  }
+              ))
+            }
+          })
+          , {name:'cart', getStorage: () => localStorage}
+    )
+    )
 
 export default useStore
