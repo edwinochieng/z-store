259d79edba48428dd364b8248b0b7df6806594ff
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-08 19:41:55 +0300

    change price type to number

--- store/store.ts
+++ store/store.ts
@@ -2,28 +2,76 @@ import { Products } from '@/utils/data'
 import { create } from 'zustand'
 import {persist} from 'zustand/middleware'
 
+export type Cart = {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    category: string;
+    quantity: number;
+}
+
 interface Store {
-  cartItems: Products[]
-  quantity:number
+  cartItems: Cart[]
   addToCart: (product:Products) => void
+  updateQuantity: (product:Products, action: 'increase' | 'decrease') => void
+  clearCart:() => void
+  removeFromCart: (product:Products) => void
 }
 
 const useStore = create<Store>()(
     persist(
-        (set) => ({
+        (set,get) => ({
             cartItems: [],
-            quantity:0,
+
             addToCart: (product) => {
-              set((state) => (
-                  {
-                      quantity: state.quantity + 1,
-                      cartItems:[...state.cartItems, product]
-                  }
-              ))
+
+               const newItem = {...product, quantity:1}
+               const cart = get().cartItems;
+
+               const foundIndex = cart.findIndex((item) => item.id === product.id);
+               if (foundIndex >= 0) {
+                 cart[foundIndex] = {
+                   ...cart[foundIndex],
+                   quantity: cart[foundIndex].quantity + 1,
+                 };
+               } else {
+                 set(() => ({
+                   cartItems: [...cart, newItem],
+                 }));
+               }
+            },
+            updateQuantity: (product,action) => {
+              const cart = get().cartItems;
+              const foundIndex = cart.findIndex((item) => item.id === product.id);
+
+              if(foundIndex >=0){
+                if (action === 'increase') {
+                  cart[foundIndex] = {
+                    ...cart[foundIndex],
+                    quantity: cart[foundIndex].quantity + 1,
+                  };
+                } else if(action === 'decrease') {
+                 cart[foundIndex] = {
+                   ...cart[foundIndex],
+                   quantity: cart[foundIndex].quantity > 1 ? cart[foundIndex].quantity - 1 : cart[foundIndex].quantity
+                 };
+                }
+              }
+              
             }
+             ,
+            
+            removeFromCart: (product) => {
+              set({cartItems: get().cartItems.filter((item) => item.id !== product.id)})
+          },
+
+          clearCart: () => set({cartItems: [] }),
+
           })
           , {name:'cart', getStorage: () => localStorage}
     )
     )
 
 export default useStore
+
