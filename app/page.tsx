aa01d544283d4711d05dcd3f60dc3e89b6e4b2d9
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-12 04:41:10 +0300

    fetch all products from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import Banner from "@/components/Banner";
 import ProductsList from "@/components/pagination/ProductsList";
 
-export default function Home() {
-  return (
-    <div>
-      <div>
-        <Banner />
-      </div>
-      <div>
-        <ProductsList />
-      </div>
-    </div>
-  );
+const getProducts = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/api/products`);
+
+  if (!res.ok) {
+    throw new Error("Data not fetched");
+  }
+
+  return res.json();
+};
+
+export default async function Home() {
+  const data = await getProducts();
+  console.log(data);
+
+  return <div>Hello</div>;
 }
---
+++ pages/api/products/index.ts
@@ -0,0 +1,20 @@
+import { prisma } from '@/prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export interface Products {
+    id:number
+    name:string
+    price:number
+    image:string
+    category:string
+    description:string
+    }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Products[]>
+) {
+    const data = await prisma.product.findMany()
+
+    res.status(200).json(data)
+}
