e9da93c64af785f00533c9602701908fd027316e
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-14 01:53:57 +0300

    edit pagination

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -23,44 +23,60 @@ export default function Pagination({
     pageNumbers.push(i);
   }
 
+  const goToPrevPage = (): void => {
+    if (currentPage > 1) {
+      previousPage();
+    }
+  };
+
+  const goToNextPage = (): void => {
+    if (currentPage < Math.ceil(totalProducts / productsPerPage)) {
+      nextPage();
+    }
+  };
+
   return (
     <div className='my-6 text-sm'>
       <ul className='inline-flex -space-x-px'>
-        {currentPage > 1 && (
-          <li>
-            <a
-              href='#products'
-              onClick={() => previousPage()}
-              className='bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 '
-            >
-              Prev
-            </a>
-          </li>
-        )}
+        <li>
+          <a
+            href='#products'
+            onClick={goToPrevPage}
+            className={`${
+              currentPage == 1
+                ? "bg-gray-100 cursor-not-allowed border border-gray-300 text-gray-400"
+                : "bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700 "
+            }border border-gray-300 ml-0 rounded-l-lg leading-tight py-2 px-3 `}
+          >
+            Prev
+          </a>
+        </li>
 
         {pageNumbers.map((number) => (
           <li key={number}>
             <a
               href='#products'
               onClick={() => paginate(number)}
-              className='bg-white border border-gray-300 text-gray-500 focus:bg-slate-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 '
+              className='bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 '
             >
               {number}
             </a>
           </li>
         ))}
 
-        {currentPage < Math.ceil(totalProducts / productsPerPage) && (
-          <li>
-            <a
-              href='#products'
-              onClick={() => nextPage()}
-              className='bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3 '
-            >
-              Next
-            </a>
-          </li>
-        )}
+        <li>
+          <a
+            href='#products'
+            onClick={goToNextPage}
+            className={`${
+              currentPage == Math.ceil(totalProducts / productsPerPage)
+                ? "bg-gray-100 cursor-not-allowed border border-gray-300 text-gray-400"
+                : "bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700 "
+            }border border-gray-300 ml-0 rounded-r-lg leading-tight py-2 px-3 `}
+          >
+            Next
+          </a>
+        </li>
       </ul>
     </div>
   );
