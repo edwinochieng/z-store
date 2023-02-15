670fbfe7628465c62e98cc0b2f26906362d5ba3e
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-15 16:44:38 +0300

    add link to categories page

--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -3,6 +3,7 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { categories } from "@/utils/data";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Categories = () => (
   <DropdownMenu.Root>
@@ -12,19 +13,18 @@ export const Categories = () => (
     <DropdownMenu.Portal>
       <DropdownMenu.Content className='bg-white w-[200px] p-1 rounded shadow-lg '>
         {categories.map((category) => (
-          <DropdownMenu.Item
-            key={category.name}
-            className='outline-none flex items-center font-medium text-sm h-[40px] mt-0.5 px-1 rounded cursor-pointer hover:bg-gray-100'
-          >
-            <Image
-              src={category.image}
-              height={40}
-              width={40}
-              alt={category.name}
-              className='pr-0.5 h-full'
-            />
-            {category.name}
-          </DropdownMenu.Item>
+          <Link href={`/categories/${category.value}`} key={category.name}>
+            <DropdownMenu.Item className='outline-none flex items-center font-medium text-sm h-[40px] mt-0.5 px-1 rounded cursor-pointer hover:bg-gray-100'>
+              <Image
+                src={category.image}
+                height={40}
+                width={40}
+                alt={category.name}
+                className='pr-0.5 h-full'
+              />
+              {category.name}
+            </DropdownMenu.Item>
+          </Link>
         ))}
       </DropdownMenu.Content>
     </DropdownMenu.Portal>
