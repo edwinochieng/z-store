5af2638523b48f7bd7b0ec1e55a98419516c17f1
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-25 21:56:25 +0300

    auth components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,83 +1,10 @@
-"use client";
-
+import Login from "@/components/auth/Login";
 import React from "react";
-import Link from "next/link";
-import { useForm } from "react-hook-form";
 
-interface LoginInputs {
-  email: string;
-  password: string;
-}
-
-export default function Login() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginInputs>();
+export default function LoginPage() {
   return (
-    <div className='max-w-[700px] mx-auto'>
-      <form className='bg-white  mt-24 lg:mt-12 mb-0 space-y-4 rounded-xl py-8 px-3 sm:px-8 shadow-2xl'>
-        <p className='text-lg font-medium'>Log in to your account</p>
-
-        <div>
-          <label htmlFor='email' className='text-sm font-medium'>
-            Email
-          </label>
-
-          <div className='relative mt-1'>
-            <input
-              {...register("email", {
-                required: "Please enter your email",
-                pattern: {
-                  value: /^[a-zA-Z0-9_.+-]+@[a-zA_Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                  message: "Please enter valid email",
-                },
-              })}
-              type='email'
-              id='email'
-              className='w-full rounded-lg border border-gray-200 p-3 sm:p-4 pr-12 text-sm focus:outline-indigo-500'
-              placeholder='Enter email'
-            />
-            {errors.email && (
-              <span className='text-red-500 pt-1'>{errors.email.message}</span>
-            )}
-          </div>
-        </div>
-
-        <div>
-          <label htmlFor='password' className='text-sm font-medium'>
-            Password
-          </label>
-
-          <div className='relative mt-1'>
-            <input
-              {...register("password", {
-                required: "Please enter your password",
-              })}
-              type='password'
-              id='password'
-              className='w-full rounded-lg border border-gray-200 p-3 sm:p-4 pr-12 text-sm focus:outline-indigo-500'
-              placeholder='Enter password'
-            />
-            {errors.password && <span>{errors.password.message}</span>}
-          </div>
-        </div>
-
-        <button
-          type='submit'
-          className='block w-full rounded-lg bg-indigo-500 px-5 py-3 text-sm font-medium text-white'
-        >
-          Sign in
-        </button>
-
-        <p className='text-center text-sm text-gray-500'>
-          No account?
-          <Link className='pl-0.5 underline text-indigo-500' href='/signup'>
-            Sign up
-          </Link>
-        </p>
-      </form>
+    <div>
+      <Login />
     </div>
   );
 }
---
+++ components/auth/Login.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
+
+interface LoginInputs {
+  email: string;
+  password: string;
+}
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInputs>();
+
+  const submitHandler = async ({ email, password }: LoginInputs) => {
+    await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+  };
+  return (
+    <div className='max-w-[700px] mx-auto'>
+      <form
+        className='bg-white  mt-24 lg:mt-12 mb-0 space-y-4 rounded-xl py-8 px-3 sm:px-8 shadow-2xl'
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <p className='text-lg font-medium'>Log in to your account</p>
+
+        <div>
+          <label htmlFor='email' className='text-sm font-medium'>
+            Email
+          </label>
+
+          <div className='relative mt-1'>
+            <input
+              {...register("email", {
+                required: "Please enter your email",
+                pattern: {
+                  value: /^[a-zA-Z0-9_.+-]+@[a-zA_Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                  message: "Please enter valid email",
+                },
+              })}
+              type='email'
+              id='email'
+              className='w-full rounded-lg border border-gray-200 p-3 sm:p-4 pr-12 text-sm focus:outline-indigo-500'
+              placeholder='Enter email'
+            />
+            {errors.email && (
+              <span className='text-red-500 pt-1'>{errors.email.message}</span>
+            )}
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor='password' className='text-sm font-medium'>
+            Password
+          </label>
+
+          <div className='relative mt-1'>
+            <input
+              {...register("password", {
+                required: "Please enter your password",
+              })}
+              type='password'
+              id='password'
+              className='w-full rounded-lg border border-gray-200 p-3 sm:p-4 pr-12 text-sm focus:outline-indigo-500'
+              placeholder='Enter password'
+            />
+            {errors.password && <span>{errors.password.message}</span>}
+          </div>
+        </div>
+
+        <button
+          type='submit'
+          className='block w-full rounded-lg bg-indigo-500 px-5 py-3 text-sm font-medium text-white'
+        >
+          Sign in
+        </button>
+
+        <p className='text-center text-sm text-gray-500'>
+          No account?
+          <Link className='pl-0.5 underline text-indigo-500' href='/signup'>
+            Sign up
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
