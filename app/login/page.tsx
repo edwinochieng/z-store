5085723afe419513c27d8c3ceeb927556e3c9263
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-02-26 02:06:33 +0300

    remove getServersSession functions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,6 @@
-import Login from "@/components/auth/Login";
 import React from "react";
+import Login from "@/components/auth/Login";
 
 export default function LoginPage() {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  return <Login />;
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,9 +2,5 @@ import SignUp from "@/components/auth/SignUp";
 import React from "react";
 
 export default function SignUpPage() {
-  return (
-    <div>
-      <SignUp />
-    </div>
-  );
+  return <SignUp />;
 }
