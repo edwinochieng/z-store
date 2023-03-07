a82d25cceecce35724ae370ff29ef6af63b33487
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-07 15:51:28 +0300

    add image domains

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
   experimental: {
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+  images: {
+    domains: ["icons8.com"],
+  },
+};
