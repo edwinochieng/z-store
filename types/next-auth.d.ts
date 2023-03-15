0b92587896471281a16a9d7f356151861c0309b9
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-15 21:34:02 +0300

    session data types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,26 +1,24 @@
-import NextAuth from "next-auth"
-import { JWT } from "next-auth/jwt"
-
+import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-    interface Session {
-      user: {
-        id: string;
-        
-      };
-    }
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+    };
+  }
 }
-  
-  
+
 declare module "next-auth/jwt" {
-    interface JWT {
-      id: string;
-     
-    }
+  interface JWT {
+    id: string;
   }
-  
+}
+
 declare module "next-auth" {
-    interface User {
-      id: string;
-    }
+  interface User {
+    id: string;
   }
+}
