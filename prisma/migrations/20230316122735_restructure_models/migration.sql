4fcdd0b909bf4ac927ee43d11bce4c147cd46191
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-16 15:28:50 +0300

    restructure db models

---
+++ prisma/migrations/20230316122735_restructure_models/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_OrderToProduct` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('NotPaid', 'Paid');
+
+-- DropForeignKey
+ALTER TABLE "_OrderToProduct" DROP CONSTRAINT "_OrderToProduct_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_OrderToProduct" DROP CONSTRAINT "_OrderToProduct_B_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "status" "Status" NOT NULL DEFAULT 'NotPaid',
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
+
+-- DropTable
+DROP TABLE "_OrderToProduct";
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
