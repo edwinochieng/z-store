c501c704cb5eaaa68426f8520d0de87f20240be2
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-18 00:03:25 +0300

    add quantity to orderItem model

---
+++ prisma/migrations/20230317210221_add_quantity_in_order_item_model/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `quantity` on the `Product` table. All the data in the column will be lost.
+  - Added the required column `quantity` to the `OrderItem` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "OrderItem" ADD COLUMN     "quantity" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "quantity";
