ee6ed71d279a20c8779e8555d9fc50aab80d03fa
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-17 21:47:57 +0300

    fetch orderItems

--- pages/api/orders/[id]/index.ts
+++ pages/api/orders/[id]/index.ts
@@ -9,6 +9,12 @@ export default async function handler(
     const order = await prisma.order.findUnique({
       where: { id: req.query.id },
     });
+
+    const orderItems = await prisma.orderItem.findMany({
+      where: { orderId: order?.id },
+      include: { product: true },
+    });
+
     res.status(200).json(order);
   } catch (err) {
     res.status(500).send({ message: "Server error" });
