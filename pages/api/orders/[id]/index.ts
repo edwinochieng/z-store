7e6e6bae5ee39d826e0a180a39d4c966afd54e8b
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-18 17:33:08 +0300

    return order details: order and orderItems

--- pages/api/orders/[id]/index.ts
+++ pages/api/orders/[id]/index.ts
@@ -15,7 +15,30 @@ export default async function handler(
       include: { product: true },
     });
 
-    res.status(200).json(order);
+    // map OrderItem records into a new array of objects with only relevant fields
+    const items = orderItems.map(({ id, quantity, product }) => ({
+      id,
+      name: product.name,
+      image: product.image,
+      price: product.price,
+      quantity,
+    }));
+
+    // merge order and orderItem data into one object
+    const orderDetails = {
+      id: order?.id,
+      fullName: order?.fullName,
+      address: order?.address,
+      city: order?.city,
+      total: order?.total,
+      status: order?.status,
+      userId: order?.userId,
+      updatedAt: order?.updatedAt,
+      createdAt: order?.createdAt,
+      items,
+    };
+
+    res.status(200).json(orderDetails);
   } catch (err) {
     res.status(500).send({ message: "Server error" });
   }
