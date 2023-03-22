0a081f34531223a09754b8f32fceb531dff5d79a
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-22 21:03:30 +0300

    add PayPalScriptProvider

--- components/provider/Provider.tsx
+++ components/provider/Provider.tsx
@@ -3,13 +3,21 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+
+const initialOptions = {
+  "client-id": process.env.PAYPAL_CLIENT_ID as string,
+  currency: "USD",
+};
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SessionProvider>
         <Toaster position='top-center' />
-        {children}
+        <PayPalScriptProvider options={initialOptions} deferLoading={true}>    
+          {children}
+        </PayPalScriptProvider>
       </SessionProvider>
     </>
   );
