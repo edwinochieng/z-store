a8c37878442e04b1afd52a24b69cf827c7a06e89
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-04-20 21:56:12 +0300

    remove type

--- pages/api/categories/[slug].ts
+++ pages/api/categories/[slug].ts
@@ -1,15 +1,13 @@
-import { prisma } from '@/prisma/client'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Products } from '../products'
-
+import { prisma } from "@/prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Products[]>
+  res: NextApiResponse
 ) {
-    
-    const data = await prisma.product.findMany({where: { category : req.query.slug }})
+  const data = await prisma.product.findMany({
+    where: { category: req.query.slug },
+  });
 
-    res.status(200).json(data)
-
+  res.status(200).json(data);
 }
