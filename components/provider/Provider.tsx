c959a3b73e96083b6705c31d5dc3f935ddc47f43
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-04-22 20:08:46 +0300

    remove PayPalScriptProvider

--- components/provider/Provider.tsx
+++ components/provider/Provider.tsx
@@ -3,20 +3,12 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
-import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
-
-const initialOptions = {
-  "client-id": process.env.PAYPAL_CLIENT_ID,
-  currency: "USD",
-};
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <PayPalScriptProvider options={initialOptions} deferLoading={true}>
-      <SessionProvider>
-        <Toaster position='top-center' />
-        {children}
-      </SessionProvider>
-    </PayPalScriptProvider>
+    <SessionProvider>
+      <Toaster position='top-center' />
+      {children}
+    </SessionProvider>
   );
 }
